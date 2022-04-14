51aa91b6873e4e2f62905d3683b88d8411d28e88
bognarp <21261487+bognarp@users.noreply.github.com>
2022-04-14 21:00:03 +0200

    refactor: change error toast to custom component

--- src/actions/errorActions.js
+++ src/actions/errorActions.js
@@ -10,5 +10,5 @@ export const alertUserError = (error) => (dispatch) => {
 
   setTimeout(() => {
     dispatch(clearErrors());
-  }, 5000);
+  }, 6000);
 };
--- src/components/layout/Error.js
+++ src/components/layout/Error.js
@@ -1,13 +1,17 @@
-import { List, ListItem, useToast, VisuallyHidden } from '@chakra-ui/react';
+import { Text, Flex, useColorModeValue, Icon, Box } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import { isObject } from 'lodash-es';
+import { HiLightningBolt } from 'react-icons/hi';
 import React from 'react';
 
 function Error() {
-  const toast = useToast();
   const getErrors = useSelector((state) => {
     return state.errors;
   });
+
+  const backgroundColor = useColorModeValue('white', 'gray.800');
+  const errorTextColor = useColorModeValue('red.500', 'red.400');
+  const textColor = useColorModeValue('gray.600', 'gray.200');
 
   if (getErrors.length !== 0) {
     let message = getErrors[0].message;
@@ -21,21 +25,35 @@ function Error() {
     }
 
     return (
-      <VisuallyHidden>
-        {toast({
-          position: 'top',
-          description: (
-            <List>
-              {message.map((m) => (
-                <ListItem key={m}>{m}</ListItem>
-              ))}
-            </List>
-          ),
-          status: 'error',
-          duration: 7000,
-          isClosable: true,
-        })}
-      </VisuallyHidden>
+      <Flex
+        maxW="sm"
+        w="full"
+        mt={5}
+        bg={backgroundColor}
+        shadow="dark-lg"
+        rounded="lg"
+        overflow="hidden"
+        position={'absolute'}
+        alignSelf={'center'}
+        zIndex={1500}
+      >
+        <Flex justifyContent="center" alignItems="center" w={12} bg="red.500">
+          <Icon as={HiLightningBolt} color="white" boxSize={6} />
+        </Flex>
+
+        <Box mx={-3} py={2} px={4}>
+          <Box mx={3}>
+            <Text color={errorTextColor} fontWeight="bold">
+              Error
+            </Text>
+            {message.map((m) => (
+              <Text key={m} color={textColor} fontSize="sm">
+                {m}
+              </Text>
+            ))}
+          </Box>
+        </Box>
+      </Flex>
     );
   }
 
